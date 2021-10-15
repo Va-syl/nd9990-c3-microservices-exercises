6398d7c5b4cee3477894ffa60d7658997bd3b009
Va-syl <nathan.winiarczyk@gmail.com>
2021-10-14 20:37:48 -0500

    change apiHost

--- project/udagram-frontend/src/environments/environment.prod.ts
+++ project/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'ac07c74f69a5d4722873bd2dbda9c992-1268196735.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
